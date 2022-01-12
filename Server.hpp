0748441840972008caee905bd0acb898ead4fc8f
rroland20 <alena_tumanova96@mail.ru>
2022-01-12 19:43:24 +0300

    add Server.cpp/hpp from Alena

--- Server.hpp
+++ Server.hpp
@@ -1,16 +1,46 @@
 #pragma once
 
 #include <iostream>
-#include "Message.hpp"
-#include "User.hpp"
-#include "Errors.hpp"
+#include <vector>
 #include <sys/socket.h>
+#include <poll.h>
 #include <string>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-class Server
-{
+#include <cstdlib>
+#include <errno.h>
+#include <fstream>
+#include <map>
+
+#include "User.hpp"
+
+class Server {
+private:
+	int					_port;
+	std::string			_password;
+	int					_listening;
+	sockaddr_in			_sockaddr;
+	std::vector<pollfd>	_fdUsers;
+	std::vector<User *>	_UsersAccept;
+
+	Server();
+	Server(const Server& copy);
+	Server	&operator=(const Server& other);
 public:
-    Server();
-    ~Server();
-    int passCmd(Message &msg, User &user);
+	Server(int port, const std::string &password);
+	virtual ~Server();
+
+	void createSocket();
+	void bindSocket();
+	void listenSocket();
+	void acceptUsers();
+
+	int getPort() const { return _port; }
+	const std::string& getPassword() const { return _password; }
+	int passCmd(Message &msg, User &user);
 };
+
+void exita(std::string msg);
